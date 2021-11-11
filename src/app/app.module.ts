55f88b142efdf7b8e0517b185fa80bdbe2a69628
Teclesim <46702214+Teclesim@users.noreply.github.com>
2021-11-11 10:18:14 +0100

    ng g c cardTemplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import { NewStockComponent } from './new-stock/new-stock.component';
+import { CardTemplateComponent } from './card-template/card-template.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewStockComponent,
+    CardTemplateComponent
   ],
   imports: [
     BrowserModule,
